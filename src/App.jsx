fea068ab90b021308a4452939e53de09a1ee1e15
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-12-26 21:23:17 +0530

    26-12-2026

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(true);
   return <></>;
 }
 
