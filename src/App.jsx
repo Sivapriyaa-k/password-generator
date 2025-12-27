369d3800770a540f8915fd2d8b08cc8e663f6828
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-12-27 22:19:31 +0530

    27-12-2025

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,26 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [pasword, setPassword] = useState("");
+  const [password, setPassword] = useState("");
 
-  return <div></div>;
+  return (
+    <div className="w-full max-w-md mx-auto shadow-md-rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-3xl font-bold py- text-center text-white">
+        Password Generator
+      </h1>
+      <div className="flex shadow rounded-lg overflow-hidden">
+        <input
+          type="text"
+          name="password"
+          id="password"
+          value={password}
+          placeholder="Password"
+          className="bg-white outline-none w-full py-1 px-3 text-gray-700"
+        />
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
